18fa399540da5e72baaad6f667eda1d858c1b985
Brook-Ayanaw <74291574+Brook-Ayanaw@users.noreply.github.com>
2025-05-31 23:57:06 +0300

    Update LogIn.jsx

--- frontend/frontend for pharmacy/src/components/LogIn.jsx
+++ frontend/frontend for pharmacy/src/components/LogIn.jsx
@@ -16,7 +16,7 @@ function LogIn() {
     }
 
     try {
-      const response = await axios.post('http://localhost:3000/login/login', {
+      const response = await axios.post('https://pharmacy-erp.onrender.com/login/login', {
         email,
         password,
       });
